50ad3cc89965ab451b3300e55afcf42612d0832e
Carina233 <910780586@qq.com>
2023-02-07 01:10:16 +0800

    测试打包，加入移动端操作杆，屏幕适配

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,8 +10,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;//角色移动速度
     public float moveStep;//角色移动步数
     public Rigidbody2D rb;//角色Rigidbody
-    Vector2 moveDir; //移动方向（即将移动）
-    
+    public Vector2 moveDir; //移动方向（即将移动）
+    //public Vector2 mobileMoveDir; //移动方向（即将移动）
+    //摇杆
+    public FixedJoystick fixedJoystick;
+    public float JoystickX;
+    public float JoystickY;
 
     Animator anim;//角色动画
  
@@ -50,6 +54,7 @@ public class PlayerController : MonoBehaviour
     {
        
         Move();
+        MobileMove();
         controlFood();
         
     }
@@ -59,7 +64,7 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Move()
     {
-        
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             
@@ -97,11 +102,16 @@ public class PlayerController : MonoBehaviour
         }
 
         
-        
-        if(!MoveCheck(moveDir))
+
+        Debug.Log("++++++++++++++++++++++++++++++++++"+moveDir);
+
+        if (!MoveCheck(moveDir))
         {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
+
+            /*horizontal = horizontal == 0 ? fixedJoystick.Horizontal : horizontal;
+            vertical = vertical == 0 ? fixedJoystick.Vertical : vertical;*/
 
             Vector2 position = transform.position;
             position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
@@ -111,8 +121,63 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    
-    void setWalkAnim(Vector2 dir)
+    void MobileMove()
+    {
+        //Mathf.Atan2()
+        JoystickX = fixedJoystick.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition.x;
+        JoystickY = fixedJoystick.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition.y;
+
+        if (Mathf.Abs(JoystickX) > 0|| Mathf.Abs(JoystickY) > 0)
+        {
+            moveDir = new Vector2(JoystickX, JoystickY);
+        }
+        
+        
+
+        
+        /*if (fixedJoystick.transform.GetChild(0).transform.position.x > 0)
+        {
+            moveDir = Vector2.right;
+            playStepSound();
+            setWalkAnim(moveDir);
+        }
+        if (fixedJoystick.transform.GetChild(0).transform.position.x < 0)
+        {
+            moveDir = Vector2.left;
+            playStepSound();
+            setWalkAnim(moveDir);
+        }
+        if (fixedJoystick.transform.GetChild(0).transform.position.y > 0)
+        {
+            moveDir = Vector2.up;
+            playStepSound();
+            setWalkAnim(moveDir);
+        }
+        if (fixedJoystick.transform.GetChild(0).transform.position.y <0)
+        {
+            moveDir = Vector2.down;
+            playStepSound();
+            setWalkAnim(moveDir);
+        }*/
+
+        Debug.Log("++++++++++++++++++++++++++++++++++" + moveDir);
+
+        if (!MoveCheck(moveDir))
+        {
+            float horizontal = fixedJoystick.Horizontal;
+            float vertical = fixedJoystick.Vertical;
+
+            Vector2 position = transform.position;
+            position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
+            position.y = position.y + moveSpeed * vertical * Time.deltaTime;
+
+            transform.position = position;
+        }
+
+    }
+
+
+        void setWalkAnim(Vector2 dir)
     {
         //anim.SetFloat("Walk", 1);
         //anim.SetFloat("X", dir.x);
@@ -213,7 +278,7 @@ public class PlayerController : MonoBehaviour
 
     }
 
-
+    
 
     /// <summary>
     /// 拿起/放下食物
@@ -585,5 +650,314 @@ public class PlayerController : MonoBehaviour
             return hit.collider.gameObject;
         }
         return null;
+    }
+
+    /// <summary>
+    /// 按钮控制物品
+    /// </summary>
+    public void btncontrolFood()
+    {
+        if (true)
+        {
+            GameObject foodInHand = checkHand();
+            GameObject placeType = placeCheck(moveDir);
+            GameObject dish = checkDish(moveDir);
+            GameObject food = foodCheck(moveDir);
+            Debug.Log(moveDir + "placeType" + placeType);
+            inHandState = InHandState.none;
+            placeState = PlaceState.none;
+
+            if (foodInHand)//手上有东西
+            {
+
+                //手上是碟子
+                if (foodInHand.transform.name == "Dish")
+                {
+                    //是空碟子吗
+                    if (foodInHand.transform.GetChild(0).childCount == 0)//是
+                    {
+                        inHandState = InHandState.EmptyDish;
+                    }
+                    else//不是
+                    {
+                        //foodInHand.layer = LayerMask.NameToLayer("Dish");
+                        inHandState = InHandState.FullDish;
+                    }
+                }
+                else if (foodInHand.transform.name == "MixingContainer")
+                {
+                    //是空吗
+                    if (foodInHand.transform.GetChild(0).childCount == 0)//是
+                    {
+                        inHandState = InHandState.EmptyContainer;
+                    }
+                    else//不是
+                    {
+                        Debug.Log("我拿着非空混合物容器");
+                        inHandState = InHandState.MixContainer;
+                    }
+                }
+                else if (foodInHand.transform.name == "Cooker")//是厨具
+                {
+                    //是空厨具吗
+                    if (foodInHand.transform.GetChild(0).childCount == 0)//是
+                    {
+                        inHandState = InHandState.EmptyCooker;
+
+                    }
+                    else//不是
+                    {
+                        inHandState = InHandState.FullCooker;
+                    }
+                }
+                else if (foodInHand.layer == LayerMask.NameToLayer("Food"))//是食物
+                {
+                    //可混合吗
+                    if (foodInHand.GetComponent<FoodManager>().stackable == false)
+                    {
+                        inHandState = InHandState.SingleFood;
+                    }
+                    else
+                    {
+                        inHandState = InHandState.StackableFood;
+                    }
+                }
+            }
+            else//手上没东西
+            {
+
+                //先判断容器包括厨具，空，非空都可以拿
+                if (dish)
+                {
+                    //面前有东西就拿起来
+                    dish.transform.SetParent(this.transform);
+                    Debug.Log("gameObject.layer" + gameObject.layer);
+                    dish.layer = gameObject.layer;
+                    return;
+                }
+                //再判断厨具，空厨具，非空厨具都可以拿
+
+                //判断食物
+                if (food)
+                {
+                    //面前有东西就拿起来
+                    food.transform.SetParent(this.transform);
+                    Debug.Log("gameObject.layer" + gameObject.layer);
+
+                    return;
+                }
+
+                //仓库里的东西可以拿
+                if (placeType && placeType.gameObject.layer == LayerMask.NameToLayer("Warehouse"))
+                {
+                    controlWarehouse(placeType);
+                    return;
+                }
+            }
+
+            /////////////////////////////
+            //前面有什么地方
+
+            //前面有容器
+            if (dish != null)
+            {
+                //是碟子
+                if (dish.layer == LayerMask.NameToLayer("Dish"))
+                {
+                    //是空碟子吗
+                    if (dish.transform.GetChild(0).childCount == 0)//是
+                    {
+                        placeState = PlaceState.EmptyDish;
+                    }
+                    else//不是
+                    {
+                        placeState = PlaceState.FullDish;
+                        //foodInHand.layer = LayerMask.NameToLayer("Dish");
+                    }
+                }
+
+                /*Debug.Log("把东西放到碟子里呀");
+                foodInHand.transform.SetParent(dish.transform.GetChild(0));
+                foodInHand.transform.position = dish.transform.GetChild(0).position;
+
+                dish.transform.GetChild(0).GetComponent<FoodMixtureController>().checkMix = true;
+
+                return;*/
+
+                //什么东西可以放进空碟子，可以放到碟子的食物，空碟子，非空碟子里面的食物。非空厨具里面的食物。
+                else if (dish.layer == LayerMask.NameToLayer("Cooker"))
+                {
+                    //是空厨具吗
+                    if (dish.transform.GetChild(0).childCount == 0)//是
+                    {
+
+                        Debug.Log("前方有空厨具");
+                        placeState = PlaceState.EmptyCooker;
+                        //check
+                    }
+                    else//不是
+                    {
+                        placeState = PlaceState.FullCooker;
+                    }
+                    //什么东西可以放进空厨具，非空碟子里面的食物。非空厨具里面的食物。
+
+                }
+                else if (dish.layer == LayerMask.NameToLayer("MixingContainer"))
+                {
+                    //是空混合容器吗
+                    if (dish.transform.GetChild(0).childCount == 0)//是
+                    {
+                        placeState = PlaceState.EmptyContainer;
+                    }
+                    else//不是
+                    {
+                        placeState = PlaceState.MixContainer;
+                    }
+
+
+                }
+
+            }
+
+
+            //前面啥地儿也没有，滚
+            else if (!placeType)
+            {
+                return;
+            }
+            //啥地儿
+            else
+            {
+                switch (LayerMask.LayerToName(placeType.layer))//根据层级获取名称
+                {
+
+                    case "Table"://普通放置桌子
+                        foodInHand.transform.SetParent(placeType.transform);
+                        foodInHand.transform.position = placeType.transform.position;
+                        if (foodInHand.layer == LayerMask.NameToLayer("Default"))
+                        {
+                            foodInHand.layer = LayerMask.NameToLayer(foodInHand.transform.name);
+                        }
+                        break;
+                    case "Device"://无厨具厨具桌,只有厨具可以放上来
+                        if (foodInHand.transform.name == "Cooker")
+                        {
+                            foodInHand.transform.SetParent(placeType.transform);
+                            foodInHand.transform.position = placeType.transform.position;
+                            foodInHand.layer = LayerMask.NameToLayer(foodInHand.transform.name);
+
+                            //check
+                        }
+                        break;
+                    case "OutputCheck"://放到出菜口
+                        if (foodInHand.name == "Dish" && foodInHand.transform.GetChild(0).childCount != 0)
+                        {
+                            foodInHand.transform.SetParent(placeType.transform);
+                            foodInHand.transform.position = placeType.transform.position;
+                        }
+                        else if (inHandState == InHandState.SingleFood)
+                        {
+                            foodInHand.transform.SetParent(placeType.transform);
+                            foodInHand.transform.position = placeType.transform.position;
+                        }
+
+
+                        break;
+
+                    case "WasteBin"://放到垃圾桶
+                        if (foodInHand.layer == LayerMask.NameToLayer("Food"))
+                        {
+                            foodInHand.transform.SetParent(placeType.transform);
+                            foodInHand.transform.position = placeType.transform.position;
+                        }
+                        else if (foodInHand.transform.GetChild(0).childCount > 0)
+                        {
+                            for (int i = foodInHand.transform.GetChild(0).childCount - 1; i >= 0; i--)
+                            {
+                                foodInHand.transform.GetChild(0).GetChild(i).position = placeType.transform.position;
+                                foodInHand.transform.GetChild(0).GetChild(i).SetParent(placeType.transform);
+                            }
+                        }
+
+                        break;
+                }
+            }
+
+            ////////根据当前拿的东西（碟子/食物/厨具/混合容器）和面前的东西（碟子，厨具，混合容器）进行处理
+
+            //拿的是空碟子，可以装起面前厨具里煮熟的食物，厨具变空
+            //拿的是非空碟子。可以倒进垃圾桶变空（拿回空碟子），或者上菜（销毁）
+
+            if (inHandState.Equals(InHandState.EmptyDish) && placeState.Equals(PlaceState.FullCooker))
+            {
+                //如果煮熟了
+                if (dish.transform.GetComponent<CookerController>().getCookerFoodState() == 1)
+                {
+                    for (int i = dish.transform.GetChild(0).childCount - 1; i >= 0; i--)
+                    {
+                        dish.transform.GetChild(0).GetChild(i).position = foodInHand.transform.position;
+                        dish.transform.GetChild(0).GetChild(i).SetParent(foodInHand.transform.GetChild(0));
+
+                    }
+                    foodInHand.transform.GetChild(0).GetComponent<DishesFoodController>().calculator
+                        = dish.transform.GetChild(0).transform.GetComponent<FoodCookedController>().calculator;
+
+                }
+
+
+            }
+
+
+            //拿的是空混合物容器。
+            //拿的是混合物容器。可以倒进垃圾桶变空（拿回空容器），或者倒进空厨具（拿回空容器，厨具变非空）
+            else if (inHandState.Equals(InHandState.MixContainer) && placeState.Equals(PlaceState.EmptyCooker))
+            {
+                //Debug.Log("混合物倒进空厨具");
+                for (int i = foodInHand.transform.GetChild(0).childCount - 1; i >= 0; i--)
+                {
+                    foodInHand.transform.GetChild(0).GetChild(i).position = dish.transform.position;
+                    foodInHand.transform.GetChild(0).GetChild(i).SetParent(dish.transform.GetChild(0).transform);
+
+                }
+                dish.transform.GetChild(0).transform.GetComponent<FoodCookedController>().calculator
+                    = foodInHand.transform.GetChild(0).GetComponent<FoodMixtureController>().calculator;
+            }
+
+            //拿的是单食物。
+            //拿的是可混合食物材料。可以放到混合物容器。
+            //拿的是需要煮的食物
+            else if (inHandState.Equals(InHandState.StackableFood) && (placeState.Equals(PlaceState.EmptyContainer) || placeState.Equals(PlaceState.MixContainer)))
+            {
+                foodInHand.transform.SetParent(dish.transform.GetChild(0));
+                foodInHand.transform.position = dish.transform.GetChild(0).position;
+
+                dish.transform.GetChild(0).GetComponent<FoodMixtureController>().checkMix = true;
+            }
+
+            //拿的是空厨具。
+            //拿的是非空厨具，可以把厨具里的食物放到面前空碟子（厨具 变空）。倒进垃圾桶。
+            else if (inHandState.Equals(InHandState.FullCooker) && placeState.Equals(PlaceState.EmptyDish))
+            {
+                /* for (int i = foodInHand.transform.GetChild(0).childCount - 1; i >= 0; i--)
+                 {
+                     foodInHand.transform.GetChild(0).GetChild(i).position = dish.transform.position;
+                     foodInHand.transform.GetChild(0).GetChild(i).SetParent(dish.transform.GetChild(0));
+                 }*/
+
+                if (foodInHand.transform.GetComponent<CookerController>().getCookerFoodState() == 1)
+                {
+                    for (int i = foodInHand.transform.GetChild(0).childCount - 1; i >= 0; i--)
+                    {
+                        foodInHand.transform.GetChild(0).GetChild(i).position = dish.transform.position;
+                        foodInHand.transform.GetChild(0).GetChild(i).SetParent(dish.transform.GetChild(0));
+                    }
+                    dish.transform.GetChild(0).transform.GetComponent<DishesFoodController>().calculator
+                        = foodInHand.transform.GetChild(0).GetComponent<FoodCookedController>().calculator;
+                }
+            }
+
+
+
+        }
     }
 }
