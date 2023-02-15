2bb72ea6fab0151beabe816acce5266fda23a451
Carina233 <910780586@qq.com>
2023-02-15 11:48:59 +0800

    UI面板

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -2,8 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CanvasController : MonoBehaviour
+public class Missile : MonoBehaviour
 {
+   
+    
+    [SerializeField]
+    private float speed=5f;
+
+    [HideInInspector]
+    public GameObject target;
+    
+    public void Move(GameObject target)
+    {
+        
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    public void Boom(GameObject target)
+    {
+        if (target == null|| Vector3.Distance(this.transform.position, target.transform.position)<1f)
+        {
+            Debug.Log("BOOM!");
+            Destroy(this.gameObject);
+        }
+    }
+
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +38,23 @@ public class CanvasController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move(target);
+
     }
+
+    // return the top-most (root) parent GameObject that contains "go"
+    // 返回go物体的根节点
+    GameObject GetTopMostParent(GameObject go)
+    {
+        Transform root;
+        root = go.transform;
+        while (root.transform.parent!=null)
+        {
+            root = root.transform.parent;
+        }
+        return root.gameObject;
+    }
+
+
+
 }
